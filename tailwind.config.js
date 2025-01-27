1889544570656bffbafbb467b83355b2f331800e
verumecom <verumecom@gmail.com>
2025-01-27 17:42:17 +0700

    Fix Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
         border: "hsl(var(--border))",
